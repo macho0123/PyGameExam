ff5522808340d2892c89e7445033994789ac5ef9
Jakub <jakub.machacekk@gmail.com>
2018-06-12 09:37:58 +0200

    renaming

--- pyGameThing.py
+++ pyGameThing.py
@@ -13,7 +13,7 @@ NOTE_MAX = 90       # G7
 NOTE_MIN = noteMinUser - 10
 FSAMP = 22050       # Sampling frequency in Hz
 FRAME_SIZE = 1024   # How many samples per frame?
-FRAMES_PER_FFT = 8 # FFT takes average across how many frames?
+FRAMES_PER_FFT = 4  # FFT takes average across how many frames?
 num_frames = 0
 
 ######################################################################
@@ -44,8 +44,7 @@ fretPosition = '150,20 160,30'.split()
 
 def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
-def note_name(n): 
-  return NOTE_NAMES[n % 12] + str(round(n/12 - 1))
+def note_name(n):      return NOTE_NAMES[n % 12] + str(round(n/12 - 1))
 
 ######################################################################
 # Ok, ready to go now.
@@ -57,6 +56,9 @@ def note_to_fftbin(n):
 
 imin = max(0, int(np.floor(note_to_fftbin(NOTE_MIN-1))))
 imax = min(SAMPLES_PER_FFT, int(np.ceil(note_to_fftbin(NOTE_MAX+1))))
+
+print(imin)
+print(imax)
 
 # Allocate space to run an FFT. 
 buf = np.zeros(SAMPLES_PER_FFT, dtype=np.float32)
@@ -72,6 +74,8 @@ stream.start_stream()
 
 # Create Hanning window function
 window = 0.5 * (1 - np.cos(np.linspace(0, 2*np.pi, SAMPLES_PER_FFT, False))) 
+print(window)
+print("WINDOW")
 
 def getPitchData():  
 
@@ -82,7 +86,7 @@ def getPitchData():
   buf[-FRAME_SIZE:] = np.fromstring(stream.read(FRAME_SIZE), np.int16)
 
   # Run the FFT on the windowed buffer
-  fft = np.fft.rfft(buf * window)
+  fft = np.fft.rfft(buf)# * window)
 
   # Get frequency of maximum response in range
   freq = (np.abs(fft[imin:imax]).argmax() + imin) * FREQ_STEP
@@ -96,7 +100,9 @@ def getPitchData():
   num_frames += 1
 
   if num_frames >= FRAMES_PER_FFT:
+    #num_frames = 0
     if (n0>noteMinUser):
+      print(freq)
       return note_name(n0)  # return n-n0 for cents (means accuracy measure)
 
   #return "a"   #Testing 101
